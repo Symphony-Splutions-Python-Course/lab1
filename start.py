21f36f30130090d5e5cb844ebdbd76d279546215
zharkothecodesamurai <zharkothecodesamurai@gmail.com>
2020-03-03 13:06:04 +0100

    second class

--- start.py
+++ start.py
@@ -9,3 +9,16 @@ for i in range (n):
 n=20
 for i in range (n):
     print(i)
+
+
+
+the_list =[1,2,3,4,5]
+the_list2=["a","b","c","d","e"]
+
+ale=zip(the_list2,the_list)
+print(ale)
+ale2=dict(ale)
+print(ale2)
+
+for item in ale2.items():
+    print(item)
