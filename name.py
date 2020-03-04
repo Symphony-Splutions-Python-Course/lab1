6812f2ce7ce67c7739804360156e052d1bb1f3d5
Vasil Taneski <vasil@taneski.com>
2020-03-04 11:01:13 +0100

    Update name.py
    removed 'different letters' criterium, count every letter.

--- name.py
+++ name.py
@@ -4,7 +4,7 @@ name = input("Enter your name... ")
 name = name.strip()
 
 # Here your job will be to print 
-# how many different letters there 
+# how many letters there 
 # are in your name, and how many 
 # of them are vowels: ['a'. 'e', 'i', 'o', 'u', 'y']
 
