b28057e118130c10beed54099e62f3fbdac13b03
Vasil Taneski <vasil@taneski.com>
2020-03-04 10:57:10 +0100

    Added one more line in the exercise

--- name.py
+++ name.py
@@ -6,5 +6,6 @@ name = name.strip()
 # Here your job will be to print 
 # how many different letters there 
 # are in your name, and how many 
-# of them are sylables.
+# of them are vowels: ['a'. 'e', 'i', 'o', 'u', 'y']
 
+print("Your'r name {} has {} letters, of which {} vowels".format(1, 2, 3))
