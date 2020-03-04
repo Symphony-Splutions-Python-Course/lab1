1243d3f93d7987ef0e05d9dbaf2a06ee5e790610
Vasil Taneski <vasil@taneski.com>
2020-03-04 10:48:55 +0100

    Added input exercise in the lab

---
+++ name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+name = input("Enter your name... ")
+name = name.strip()
+
+# Here your job will be to print 
+# how many different letters there 
+# are in your name, and how many 
+# of them are sylables.
+
