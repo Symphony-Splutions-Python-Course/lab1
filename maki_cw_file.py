a7ebdaaf4d2caa078e17102d05b5aa85d251227e
Mladen Tasevski <mladen.tasevski@gmail.com>
2020-03-04 13:14:16 +0100

    adoasdiasdjj

--- maki_cw_file.py
+++ maki_cw_file.py
@@ -2,7 +2,7 @@ f = open("name.py", "r")
 file_content = f.read().__str__()
 
 num_lines = file_content.count("\n")
-words = file_content.count(" ") + 1
+words = file_content.count(" ") 
 letters = file_content.count("")
 
 print("Words: {}\nLines: {}\nWords: {}".format(words, num_lines, letters))
