e7aeacbedd68619f422d60c7fdeb518f4d994a1a
Mladen Tasevski <mladen.tasevski@gmail.com>
2020-03-05 15:00:24 +0100

    Check if filename is file`

--- wc.py
+++ wc.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
-
+import os
 
 def main():
     if len(sys.argv) > 1:
         for filename in sys.argv[1:]:
+            if not os.path.isfile(filename):
+                continue
             text_stats = stats(filename)
             try:
                 print(" {}  {} {} {}".format(*text_stats))
