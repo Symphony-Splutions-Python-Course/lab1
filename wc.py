de986ec5cb9d4b014e6e60ff06f9449841ca2938
Vasil Taneski <vasil@taneski.com>
2020-03-05 11:04:06 +0100

    Except two potential errors:

--- wc.py
+++ wc.py
@@ -4,15 +4,11 @@ import sys
 
 
 def main():
+    # import ipdb; ipdb.set_trace()
     if len(sys.argv) > 1:
         for filename in sys.argv[1:]:
-            print(filename)
-            #here we should go for every given filename.
-        exit()
-
-        filename = sys.argv[1]
-        text_stats = stats(filename)
-        print(" {}  {} {} {}".format(*text_stats))
+            text_stats = stats(filename)
+            print(" {}  {} {} {}".format(*text_stats))
     else:
         print("HERE COMES SYS INPUT!")
 
@@ -23,6 +19,8 @@ def stats(filename):
     - size of the file,
     - file name
     """
+
+    # import ipdb; ipdb.set_trace()
     try:
         with open(filename, "r") as file:
             file_content = file.read()
@@ -31,14 +29,19 @@ def stats(filename):
 
             lines_count = len(lines)
             words_count = len(words)
-            text_stats = stats(filename)
 
             file_size = file.seek(0, 2)
             file_name = file.name
 
-        return lines_count, words_count, file_size, file_name
+        return [lines_count, words_count, file_size, file_name]
     except FileNotFoundError as fnf_error:
-        print(fnf_error)
+        print("{}: {}".format(fnf_error.args[1], fnf_error.filename))
+        # exit()
+    except PermissionError as perm_error:
+        print("{}: {}".format(perm_error.args[1], perm_error.filename))
+        # exit()
+    else:
+        print("DONE")
 
 if __name__ == "__main__":
     main()
