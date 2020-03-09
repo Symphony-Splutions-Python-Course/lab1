8e0861f9d721453e6faac43a28cc15bffb0b7bcd
Marin Gligorov <gligorovmarin@gmail.com>
2020-03-09 14:29:12 +0100

    .

--- wc_shebang.py
+++ wc_shebang.py
@@ -5,19 +5,39 @@ import sys
 import os
 
 def main():
-    if len(sys.argv)>1:
-        for filename in sys.argv[1:]:
-            print("zdr")
-            if not os.path.isfile(filename):
-                print(filename, "is not a file")
-                continue
+    #!/usr/bin/python3
+import sys
+import os
+py_shebang = ""
+bash_shebang = ""
+files = sys.argv[1:]
+
+def add_shebang(file_name, file_type):
+    opened_file = open(file_name, "r+")
+    content = opened_file.read()
+    print(content)
+    if content.split("\n")[0].__contains__("#!/"):
+        pass
     else:
-        content = sys.stdin.read()
-        text = adding(content)
-        print(text)
+        opened_file.seek(0, 0)
+        if file_type=="py":
+            opened_file.write("#!/usr/bin/python3\n")
+        if file_type in ["sh", "bash"]:
+            opened_file.write("#!/bin/bash\n")
+        opened_file.write(content)
+    opened_file.close()
 
 
-def adding():
+for file_name in files:
+    extension = file_name.split(".")[1]
+    if extension == "py":
+        add_shebang(file_name, "py")
+    elif extension in ["sh", "bash"]:
+        add_shebang(file_name, "sh")
+    else:
+        pass
+
+def preprend():
     try:
         with open(filename,"w+") as file:
             content = file.read()
