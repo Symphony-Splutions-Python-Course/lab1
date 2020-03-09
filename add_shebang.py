0641a2a1364e9ed563e262c58b7d896998abaa09
Mladen Tasevski <mladen.tasevski@gmail.com>
2020-03-09 14:02:45 +0100

    add_shebang works

--- add_shebang.py
+++ add_shebang.py
@@ -1,18 +1,23 @@
+#!/usr/bin/python3
 import sys
 import os
+py_shebang = ""
+bash_shebang = ""
 files = sys.argv[1:]
 
 def add_shebang(file_name, file_type):
-    opened_file = open(file_name, "a+")
+    opened_file = open(file_name, "r+")
     content = opened_file.read()
-    if content.__contains__("#!/"):
+    print(content)
+    if content.split("\n")[0].__contains__("#!/"):
         pass
     else:
-        opened_file.seek(0)
+        opened_file.seek(0, 0)
         if file_type=="py":
-            opened_file.write("#!/usr/bin/python3\n" + content)
+            opened_file.write("#!/usr/bin/python3\n")
         if file_type in ["sh", "bash"]:
-            opened_file.write("#!/bin/bash\n" + content)
+            opened_file.write("#!/bin/bash\n")
+        opened_file.write(content)
     opened_file.close()
 
 
@@ -24,3 +29,4 @@ for file_name in files:
         add_shebang(file_name, "sh")
     else:
         pass
+
